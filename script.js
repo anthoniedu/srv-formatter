ea38c00088f71f21c84e1217bcd789a638fa7184
Anthoni <anthoniedu17@gmail.com>
2021-12-17 00:42:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var tag = document.getElementById("tag");
+var result = document.getElementById("result");
+var entregue = document.getElementById("entregue");
+
+function show() {
+    result.value = format();
+
+    tag.value = "";
+    entregue.value = "";
+
+    copiar();
+}
+        
+function concatenado() {
+    var texto = ` (ENTREGUE A ${entregue.value})  `;
+
+    return texto;
+}
+
+function copiar() {
+    var newtext = document.getElementById('result');
+    newtext.select();
+    document.execCommand('copy');
+}
+
+function format() {
+    var text2 = tag.value.replace(' (', ': ');
+    text2 = text2.replace(') ',' ');
+    text2 = text2.replace(' (', ': ');
+    text2 = text2.replace(') ', '.\n\n');
+    text2 = text2.replace('CLIENTE (', '');
+    text2 = text2.replace(')', '');
+    text2 = text2.replace('.', concatenado().toUpperCase());
+
+    return text2;
+}
