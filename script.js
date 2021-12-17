90dd168743a01e930220c487f6a6ff269539944d
Anthoni <64325502+roovap@users.noreply.github.com>
2021-12-17 14:02:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ function copiar() {
 
 function format() {
     var text2 = tag.value.replace(' (', ': ');
-    text2 = text2.replace(') / ',' ');
-    text2 = text2.replace(' (', ': ');
+    text2 = text2.replace(') / TAG (',' ');
     text2 = text2.replace(') ', '.\n\n');
     text2 = text2.replace('/ Cliente (', '');
     text2 = text2.replace(')', '');
